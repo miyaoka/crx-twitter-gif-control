34b090c42ed4800aba3e25706c1a986d799d1487
Masaya Kazama <miyan@t-p.jp>
2022-05-21 20:20:48 +0900

    twitterのvideoPlayer表示時にcontrolsを追加し、前面レイヤーのクリックを無効化する

--- src/content-scripts/main.ts
+++ src/content-scripts/main.ts
@@ -1,1 +1,24 @@
-console.log("content scripts");
+const addVideoControl = () => {
+  document
+    .querySelectorAll('[data-testid="videoPlayer"]')
+    .forEach((videoPlayer) => {
+      const video = videoPlayer.querySelector("video");
+      const control = videoPlayer.querySelector<HTMLElement>('[tabindex="0"]');
+      video?.setAttribute("controls", "");
+
+      if (control) {
+        control.style.pointerEvents = "none";
+      }
+    });
+};
+
+new PerformanceObserver(() => {
+  addVideoControl();
+}).observe({
+  type: "longtask",
+  buffered: true,
+});
+
+document.addEventListener("scroll", () => {
+  addVideoControl();
+});
